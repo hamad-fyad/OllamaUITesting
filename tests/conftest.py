45f528e2080137815f7e40d1b04d6ff209ea0cde
hamad <Hamad.fyad.05@gmail.com>
2025-09-11 09:41:11 +0300

    sdsd

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,6 @@ def pytest_configure(config):
         f.write(f"Target.URL={os.getenv('OLLAMA_URL', 'NOT_SET')}\n")
         f.write(f"Ollama.Image={os.getenv('OLLAMA_UI_IMAGE', os.getenv('OLLAMA_UI_IMG_TAG', 'NOT_SET'))}\n")
         f.write(f"YOLO.Image={os.getenv('YOLO_IMAGE', os.getenv('YOLO_IMG_TAG', 'NOT_SET'))}\n")
-        f.write(f"ollama_img_tag={os.getenv('ollama_img_tag', 'NOT_SET')}\n")
         f.write(f"postgres_version={os.getenv('postgres_version', 'NOT_SET')}\n")
 
 
